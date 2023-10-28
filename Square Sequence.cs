38779f606d3ee9eba0cad732c245afc492c5d41e
Gold Beluga <88539712+GoldBeluga@users.noreply.github.com>
2023-10-28 11:38:11 +0800

    Create Square Sequence.cs

---
+++ Square Sequence.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Program;
+public static class Program
+{
+    static void Main()
+    {
+        Console.WriteLine((int.Parse(Console.ReadLine()) - 2) / 2);
+    }
+
+}
